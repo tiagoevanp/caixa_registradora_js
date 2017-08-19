5aef06bbbf515dbfdabf0bf16f82fe22a1d4cb58
tiagoevanp <tiagoevanp@hotmail.com>
2017-08-19 16:06:54 -0300

    Adicionando o programa em Javascript que roda na página!

---
+++ registradora.js
@@ -0,0 +1,35 @@
+var conta = [];	
+
+ 	function adicionar_item(){
+ 		var total = valor.value*quantidade.value;
+
+  		var f = document.createTextNode(quantidade.value+"x "+item.value+" | Preço unitário: R$ "+valor.value+" | Preço: R$ "+total);
+  		var elemento1 = document.createElement("P");
+  		elemento1.appendChild(f);
+  		document.getElementById("compra").appendChild(elemento1);
+  		
+  		conta.push(total);
+  	}
+	
+	function imprimir_total(){
+		var i = 0
+		var soma = 0;
+	
+		for(var i = 0; i < conta.length; i++){
+  			soma+=conta[i];
+  		}
+
+		var t = document.createTextNode("Total: R$ "+ soma.toFixed(2));
+  		var elemento2 = document.createElement("P");
+    	elemento2.appendChild(t);
+    	document.getElementById("compra").appendChild(elemento2);
+	}
+
+	function limpar_registros(){
+		var resposta = confirm("Você tem certeza que quer zerar os registros?");
+    if(resposta === true){
+      conta = [];
+		  document.getElementById("compra").innerHTML = "";
+    }
+	}
+
