d8d6913de51ed184cc9b4c20069dd30d828f4810
Tiago Evan <tiagoevanp@hotmail.com>
2018-06-11 01:29:06 -0300

    All in the way again

--- js/registradora.js
+++ js/registradora.js
@@ -1,43 +1,52 @@
-    //Initialization of tooltips
-    document.addEventListener('DOMContentLoaded', function() {
-      var elems = document.querySelectorAll('.tooltipped');
-      console.log(elems)
-      var instances = M.Tooltip.init(elems);
-    });
+//Initialization of tooltips
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.tooltipped');
+  var instances = M.Tooltip.init(elems);
+});
 
-    var conta = []; 
-    
-    function adicionar_item(){
-      var total = valor.value*quantidade.value;
-      
-      var f = document.createTextNode(quantidade.value+"x "+item.value+" | Preço unitário: R$ "+valor.value+" | Preço: R$ "+total.toFixed(2));
-      var elemento1 = document.createElement("P");
-      elemento1.appendChild(f);
-      document.getElementById("compra").appendChild(elemento1);
-      
-      conta.push(total);
-  	}
-	
-    function imprimir_total(){
-		  var i = 0
-		  var soma = 0;
-		  
-      for(var i = 0; i < conta.length; i++){
-  		    soma+=conta[i];
-  		}
+function maskMoney() {
+  money = document.getElementById('value').value
+  document.getElementById('value').value = Number(money).toFixed(2);
+}
 
-		  var t = document.createTextNode("Total: R$ "+ soma.toFixed(2));
-      var elemento2 = document.createElement("P");
-      elemento2.appendChild(t);
-      document.getElementById("compra").appendChild(elemento2);
-    }
+var conta = []; 
 
-    function limpar_registros(){
-		  var resposta = confirm("Você tem certeza que quer zerar os registros?");
-      
-      if(resposta === true){
-        conta = [];
-		    document.getElementById("compra").innerHTML = "";
-      }
-    }
+function registerItem(){
+  if (quantity.value == "") {
+    quantity.value = 1
+  }
+  if (item.value == "") {
+    item.value = "something";
+  }
+  if (value.value == "") {
+    value.value = 0;
+  }
+  var total = value.value*quantity.value;
+  var f = document.createTextNode(quantity.value+"x "+item.value+" | Price: $ "+value.value);
+  var elemento1 = document.createElement("P");
+  elemento1.appendChild(f);
+  document.getElementById("invoice").appendChild(elemento1);
+  
+  conta.push(total);
+}
+
+function showTotal(){
+  var soma = 0;
+  
+  for(var i = 0; i < conta.length; i++){
+	    soma += conta[i];
+	}
+
+  var t = document.createTextNode("Total: R$ "+ soma.toFixed(2));
+  var elemento2 = document.createElement("P");
+  elemento2.appendChild(t);
+  document.getElementById("invoice").appendChild(elemento2);
+}
+
+function cleanInvoice(){
+  if(confirm("Você tem certeza que quer zerar os registros?")){
+    conta = [];
+    document.getElementById("invoice").innerHTML = "";
+  }
+}
 
